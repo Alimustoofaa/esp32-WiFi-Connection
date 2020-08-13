6a537c52f27a56e638e9f0da229f8e254fa6ea6f
Ali Mustofa <56061857+Alimustoofaa@users.noreply.github.com>
2020-08-13 22:47:13 +0700

    Create esp32_wifi_connection.ino

---
+++ esp32_wifi_connection.ino
@@ -0,0 +1,27 @@
+#include "WiFi.h"
+
+char* ssid              = "Nama_WiFI";
+char* password          = "Password_WiFi";
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  int reset = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    reset ++;
+    Serial.print("Menyambungkan ke WiFi : ");
+    Serial.println(ssid);
+    if (reset == 5) {
+      ESP.restart();
+    }
+  }
+  Serial.print("Berhasil Menyambungkan ke WiFi : ");
+  Serial.println(ssid);
+  Serial.print("IP Address : ");
+  Serial.println(WiFi.localIP());
+}
+.
+
+void loop(){
+}
